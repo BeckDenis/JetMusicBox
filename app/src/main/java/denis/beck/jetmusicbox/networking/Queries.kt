fa84e1c05792385bb73ab257e6b74d235c1a812c
denis <d.beck@bsl.dev>
2021-11-07 01:59:32 +0600

    Add Queries.kt

---
+++ app/src/main/java/denis/beck/jetmusicbox/networking/Queries.kt
@@ -0,0 +1,9 @@
+package denis.beck.jetmusicbox.networking
+
+object Queries {
+    const val responseType = "response_type"
+    const val clientId = "client_id"
+    const val scope = "scope"
+    const val redirectUri = "redirect_uri"
+    const val code = "code"
+}
