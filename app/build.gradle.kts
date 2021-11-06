c0b1a7bc07a83d0557db2f0790f7aae813985dbe
denis <d.beck@bsl.dev>
2021-11-06 15:30:38 +0600

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,11 +35,36 @@ android {
 }
 
 dependencies {
+    // Android
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.1")
+    implementation("com.google.android.material:material:1.4.0")
+
+    // Jetpack Compose
+    implementation("androidx.compose.ui:ui:1.0.5")
+    implementation("androidx.compose.ui:ui-tooling:1.0.5")
+    implementation("androidx.compose.foundation:foundation:1.0.5")
+
+    implementation("androidx.compose.material:material:1.0.5")
+    implementation("androidx.compose.material:material-icons-core:1.0.5")
+    implementation("androidx.compose.material:material-icons-extended:1.0.5")
+
+    implementation("androidx.compose.runtime:runtime-livedata:1.0.5")
+
+    // LifeCycle
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.4.0")
+
+    /** Test */
     testImplementation("junit:junit:4.+")
+
+    /** Android Test */
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+
+    // Compose
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.5")
 }
