109746ca3f4a064035dfa5aa62111abc276efb9e
denis <d.beck@bsl.dev>
2021-11-07 14:50:21 +0600

    Add AuthApi.kt

---
+++ app/src/main/java/denis/beck/jetmusicbox/networking/apis/AuthApi.kt
@@ -0,0 +1,20 @@
+package denis.beck.jetmusicbox.networking.apis
+
+import denis.beck.jetmusicbox.BuildConfig
+import denis.beck.jetmusicbox.networking.GrantTypes
+import denis.beck.jetmusicbox.networking.Queries
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface AuthApi {
+
+    @FormUrlEncoded
+    @POST("/api/token")
+    suspend fun accessToken(
+        @Field(Queries.code) code: String,
+        @Field(Queries.redirectUri) redirectUri: String = BuildConfig.redirectURI,
+        @Field(Queries.grantType) grantType: String = GrantTypes.authorizationCode
+    )
+
+}
