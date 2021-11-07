766a38dd9d7c93d5c32c7b1b67cbe5985e0b61d1
denis <d.beck@bsl.dev>
2021-11-07 15:13:16 +0600

    Update AuthApi.kt

--- app/src/main/java/denis/beck/jetmusicbox/networking/apis/AuthApi.kt
+++ app/src/main/java/denis/beck/jetmusicbox/networking/apis/AuthApi.kt
@@ -3,6 +3,7 @@ package denis.beck.jetmusicbox.networking.apis
 import denis.beck.jetmusicbox.BuildConfig
 import denis.beck.jetmusicbox.networking.constants.GrantTypes
 import denis.beck.jetmusicbox.networking.constants.Queries
+import denis.beck.jetmusicbox.networking.responses.AuthResponse
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
@@ -15,6 +16,6 @@ interface AuthApi {
         @Field(Queries.code) code: String,
         @Field(Queries.redirectUri) redirectUri: String = BuildConfig.redirectURI,
         @Field(Queries.grantType) grantType: String = GrantTypes.authorizationCode
-    )
+    ) : AuthResponse
 
 }
