1b8b479891ead19f16dc12a466af0ed1f272263c
denis <d.beck@bsl.dev>
2021-11-07 10:59:00 +0600

    Add hilt dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,10 @@
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 
 plugins {
+    kotlin("kapt")
     id("com.android.application")
     id("kotlin-android")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -94,6 +96,10 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0")
     implementation("androidx.lifecycle:lifecycle-common-java8:2.4.0")
 
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.38.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.38.1")
+
     /** Test */
     testImplementation("junit:junit:4.+")
 
@@ -103,4 +109,9 @@ dependencies {
 
     // Compose
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.5")
+}
+
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
 }
