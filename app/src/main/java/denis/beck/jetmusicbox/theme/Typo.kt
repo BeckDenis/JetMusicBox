9dc0719a078875db11b205b6818bd74fbdc2988f
denis <d.beck@bsl.dev>
2021-11-09 10:11:31 +0600

    Add Typo.kt

---
+++ app/src/main/java/denis/beck/jetmusicbox/theme/Typo.kt
@@ -0,0 +1,14 @@
+package denis.beck.jetmusicbox.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import denis.beck.jetmusicbox.R
+
+private val CaviarDreams = FontFamily(
+    Font(R.font.caviar_dreams_regular, FontWeight.Normal),
+    Font(R.font.caviar_dreams_bold, FontWeight.Bold)
+)
+
+val MyTypography = Typography()
