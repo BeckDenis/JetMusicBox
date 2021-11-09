47309f63502045cbc0f749bcfdf291e483db1db5
denis <d.beck@bsl.dev>
2021-11-09 11:32:09 +0600

    Update Color.kt

--- app/src/main/java/denis/beck/jetmusicbox/theme/Color.kt
+++ app/src/main/java/denis/beck/jetmusicbox/theme/Color.kt
@@ -5,3 +5,4 @@ import androidx.compose.ui.graphics.Color
 val GrayishRedPurple = Color(0xFF7F5358)
 val VintagePink = Color(0xFFC37F84)
 val EerieBlack = Color(0xFF1A1720)
+val PearlWhite = Color(0xFFF3EEE2)
