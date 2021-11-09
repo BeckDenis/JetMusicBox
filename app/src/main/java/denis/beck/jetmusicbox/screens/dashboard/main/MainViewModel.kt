8ead41b021f7bc753b381ccfa7ed30a10a625fc1
denis <d.beck@bsl.dev>
2021-11-09 23:42:45 +0600

    Update MainViewModel.kt

--- app/src/main/java/denis/beck/jetmusicbox/screens/dashboard/main/MainViewModel.kt
+++ app/src/main/java/denis/beck/jetmusicbox/screens/dashboard/main/MainViewModel.kt
@@ -1,10 +1,13 @@
 package denis.beck.jetmusicbox.screens.dashboard.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import denis.beck.jetmusicbox.repositories.albums.AlbumsRepository
 import denis.beck.jetmusicbox.repositories.playlists.PlaylistsRepository
+import denis.beck.jetmusicbox.screens.dashboard.main.models.MainUiState
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
@@ -14,12 +17,16 @@ class MainViewModel @Inject constructor(
     albumsRepository: AlbumsRepository,
     playlistsRepository: PlaylistsRepository,
 ) : ViewModel() {
+
+    private val _uiState = MutableLiveData<MainUiState>(MainUiState.Loading)
+    val uiState: LiveData<MainUiState> = _uiState
+
     init {
         viewModelScope.launch {
             // Todo: Add global error handler
             try {
-                albumsRepository.getNewReleases()
-                playlistsRepository.getFeaturedPlaylist()
+                val albums = albumsRepository.getNewReleases()
+                _uiState.postValue(MainUiState.Idle(albums.albums.items))
             } catch (e: Exception) {
                 Timber.e(e.message)
             }
