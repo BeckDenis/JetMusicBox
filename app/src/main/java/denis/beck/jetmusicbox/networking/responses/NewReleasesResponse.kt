f4514c0de572fcb9b6489bb4c7c3926fa0b44430
denis <d.beck@bsl.dev>
2021-11-09 17:46:12 +0600

    Update NewReleasesResponse.kt

--- app/src/main/java/denis/beck/jetmusicbox/networking/responses/NewReleasesResponse.kt
+++ app/src/main/java/denis/beck/jetmusicbox/networking/responses/NewReleasesResponse.kt
@@ -8,7 +8,7 @@ data class NewReleasesResponse(
 
 data class Albums(
     @SerialName("href") val href: String,
-    @SerialName("items") val items: List<Items>,
+    @SerialName("items") val items: List<Album>,
     @SerialName("limit") val limit: Int,
     @SerialName("next") val next: String,
     @SerialName("offset") val offset: Int,
@@ -16,24 +16,24 @@ data class Albums(
     @SerialName("total") val total: Int,
 )
 
-data class Items(
-    @SerialName("album_type") val album_type: String,
-    @SerialName("artists") val artists: List<Artists>,
-    @SerialName("available_markets") val available_markets: List<String>,
-    @SerialName("external_urls") val external_urls: ExternalUrls,
+data class Album(
+    @SerialName("album_type") val albumType: String,
+    @SerialName("artists") val artists: List<Artist>,
+    @SerialName("available_markets") val availableMarkets: List<String>,
+    @SerialName("external_urls") val externalUrl: ExternalUrl,
     @SerialName("href") val href: String,
     @SerialName("id") val id: String,
     @SerialName("images") val images: List<Images>,
     @SerialName("name") val name: String,
-    @SerialName("release_date") val release_date: String,
-    @SerialName("release_date_precision") val release_date_precision: String,
-    @SerialName("total_tracks") val total_tracks: Int,
+    @SerialName("release_date") val releaseDate: String,
+    @SerialName("release_date_precision") val releaseDatePrecision: String,
+    @SerialName("total_tracks") val totalTracks: Int,
     @SerialName("type") val type: String,
     @SerialName("uri") val uri: String,
 )
 
-data class Artists(
-    @SerialName("external_urls") val external_urls: ExternalUrls,
+data class Artist(
+    @SerialName("external_urls") val externalUrl: ExternalUrl,
     @SerialName("href") val href: String,
     @SerialName("id") val id: String,
     @SerialName("name") val name: String,
@@ -41,7 +41,7 @@ data class Artists(
     @SerialName("uri") val uri: String,
 )
 
-data class ExternalUrls(
+data class ExternalUrl(
     @SerialName("spotify") val spotify: String,
 )
 
