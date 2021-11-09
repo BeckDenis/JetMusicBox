3a3379983a4aaf4cfbadfc66265bc5c105565af1
denis <d.beck@bsl.dev>
2021-11-09 17:34:05 +0600

    Update MainViewModel.kt

--- app/src/main/java/denis/beck/jetmusicbox/screens/dashboard/main/MainViewModel.kt
+++ app/src/main/java/denis/beck/jetmusicbox/screens/dashboard/main/MainViewModel.kt
@@ -5,13 +5,19 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import denis.beck.jetmusicbox.repositories.albums.AlbumsRepository
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(albumsRepository: AlbumsRepository) : ViewModel() {
     init {
         viewModelScope.launch {
-            albumsRepository.getNewReleases()
+            // Todo: Add global error handler
+            try {
+                albumsRepository.getNewReleases()
+            } catch (e: Exception) {
+                Timber.e(e.message)
+            }
         }
     }
 }
