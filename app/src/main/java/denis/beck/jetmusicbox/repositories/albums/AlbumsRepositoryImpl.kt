d37772b1c654d83894d99e8e94ce5bc72816f3ff
denis <d.beck@bsl.dev>
2021-11-09 16:29:18 +0600

    Add AlbumsRepositoryImpl.kt

---
+++ app/src/main/java/denis/beck/jetmusicbox/repositories/albums/AlbumsRepositoryImpl.kt
@@ -0,0 +1,9 @@
+package denis.beck.jetmusicbox.repositories.albums
+
+import denis.beck.jetmusicbox.networking.apis.SpotifyApi
+import javax.inject.Inject
+
+class AlbumsRepositoryImpl @Inject constructor(private val spotifyApi: SpotifyApi) :
+    AlbumsRepository {
+    override suspend fun getNewReleases() = spotifyApi.getNewReleases()
+}
