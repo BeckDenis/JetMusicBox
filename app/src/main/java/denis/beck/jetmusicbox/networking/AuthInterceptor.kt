f64938640f3fc67c63f1856031d3a8ef6459cf65
denis <denisbeck555@gmail.com>
2021-11-10 18:14:58 +0600

    Update AuthInterceptor.kt

--- app/src/main/java/denis/beck/jetmusicbox/networking/AuthInterceptor.kt
+++ app/src/main/java/denis/beck/jetmusicbox/networking/AuthInterceptor.kt
@@ -1,21 +1,16 @@
 package denis.beck.jetmusicbox.networking
 
-import android.util.Base64
 import denis.beck.jetmusicbox.BuildConfig
+import okhttp3.Credentials
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class AuthInterceptor : Interceptor {
 
-    private val base64EncodedAuth: String
-        get() {
-            val value = "${BuildConfig.clientId}:${BuildConfig.clientSecret}"
-            return Base64.encodeToString(value.toByteArray(), Base64.URL_SAFE + Base64.NO_WRAP)
-        }
-
     override fun intercept(chain: Interceptor.Chain): Response {
+        val credential = Credentials.basic(BuildConfig.clientId, BuildConfig.clientSecret)
         val request = chain.request().newBuilder()
-            .addHeader("Authorization", "Basic $base64EncodedAuth")
+            .addHeader("Authorization", credential)
             .build()
 
         return chain.proceed(request)
