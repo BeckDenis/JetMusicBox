31df062fb853de41c02e45c7042406ad8980a2a2
denis <denisbeck555@gmail.com>
2021-11-14 00:21:16 +0600

    Update AuthWebEvent.kt

--- app/src/main/java/denis/beck/jetmusicbox/screens/authentication/web/models/AuthWebEvent.kt
+++ app/src/main/java/denis/beck/jetmusicbox/screens/authentication/web/models/AuthWebEvent.kt
@@ -2,5 +2,6 @@ package denis.beck.jetmusicbox.screens.authentication.web.models
 
 import denis.beck.jetmusicbox.core.viewmodel.UiEvent
 
-class AuthWebEvent : UiEvent {
+sealed class AuthWebEvent : UiEvent {
+    object UserAuthorized : AuthWebEvent()
 }
