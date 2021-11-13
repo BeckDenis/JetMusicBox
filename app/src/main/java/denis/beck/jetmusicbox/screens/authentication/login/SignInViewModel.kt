2567fd4d5d98aae79eaa91a491bfe285d5aa2aab
denis <denisbeck555@gmail.com>
2021-11-13 23:36:29 +0600

    Update SignInViewModel.kt

--- app/src/main/java/denis/beck/jetmusicbox/screens/authentication/login/SignInViewModel.kt
+++ app/src/main/java/denis/beck/jetmusicbox/screens/authentication/login/SignInViewModel.kt
@@ -1,27 +1,38 @@
 package denis.beck.jetmusicbox.screens.authentication.login
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import denis.beck.jetmusicbox.core.viewmodel.BaseViewModel
 import denis.beck.jetmusicbox.managers.auth.AuthManager
-import denis.beck.jetmusicbox.screens.authentication.login.models.SignInUiState
+import denis.beck.jetmusicbox.screens.authentication.login.models.SignInEvent
+import denis.beck.jetmusicbox.screens.authentication.login.models.SignInSideEffect
+import denis.beck.jetmusicbox.screens.authentication.login.models.SignInState
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SignInViewModel @Inject constructor(authManager: AuthManager) : ViewModel() {
-
-    private val _uiState = MutableLiveData<SignInUiState>(SignInUiState.AuthChecking)
-    val uiState: LiveData<SignInUiState> = _uiState
+class SignInViewModel @Inject constructor(authManager: AuthManager) :
+    BaseViewModel<SignInEvent, SignInState, SignInSideEffect>() {
 
     init {
-        viewModelScope.launch {
-            if (authManager.isAuthorize()) {
-                _uiState.postValue(SignInUiState.Authorized)
-            } else {
-                _uiState.postValue(SignInUiState.Idle)
+        viewModelScope.launch(Dispatchers.IO) { checkAuth(authManager) }
+    }
+
+    private suspend fun checkAuth(authManager: AuthManager) {
+        if (authManager.isAuthorize()) {
+            setEffect { SignInSideEffect.Navigation.ToMain }
+        } else {
+            setState { SignInState.Idle }
+        }
+    }
+
+    override fun setInitialState() = SignInState.AuthChecking
+
+    override fun handleEvents(event: SignInEvent) {
+        when (event) {
+            is SignInEvent.SignInButtonClick -> setEffect {
+                SignInSideEffect.Navigation.ToAuthWeb
             }
         }
     }
--- app/src/main/java/denis/beck/jetmusicbox/screens/authentication/login/models/SignInUiState.kt
+++
@@ -1,7 +0,0 @@
-package denis.beck.jetmusicbox.screens.authentication.login.models
-
-sealed class SignInUiState {
-    object AuthChecking: SignInUiState()
-    object Idle: SignInUiState()
-    object Authorized: SignInUiState()
-}
