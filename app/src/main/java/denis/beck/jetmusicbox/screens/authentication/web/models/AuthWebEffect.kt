671ae5f3873cc4cdafa8a168279ba3c1e463a98b
denis <denisbeck555@gmail.com>
2021-11-14 00:21:06 +0600

    Update AuthWebEffect.kt

--- app/src/main/java/denis/beck/jetmusicbox/screens/authentication/web/models/AuthWebEffect.kt
+++ app/src/main/java/denis/beck/jetmusicbox/screens/authentication/web/models/AuthWebEffect.kt
@@ -2,5 +2,8 @@ package denis.beck.jetmusicbox.screens.authentication.web.models
 
 import denis.beck.jetmusicbox.core.viewmodel.UiSideEffect
 
-class AuthWebEffect : UiSideEffect {
+sealed class AuthWebEffect : UiSideEffect {
+    sealed class Navigate: AuthWebEffect() {
+        object ToMain: Navigate()
+    }
 }
