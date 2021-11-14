8b7e62840f0dde7f4f94902b4e9626736ce6699d
denis <denisbeck555@gmail.com>
2021-11-14 12:55:53 +0600

    Update MainState.kt

--- app/src/main/java/denis/beck/jetmusicbox/screens/dashboard/main/models/MainState.kt
+++ app/src/main/java/denis/beck/jetmusicbox/screens/dashboard/main/models/MainState.kt
@@ -1,9 +1,10 @@
 package denis.beck.jetmusicbox.screens.dashboard.main.models
 
+import denis.beck.jetmusicbox.core.viewmodel.UiState
 import denis.beck.jetmusicbox.networking.responses.AlbumResponse
 import denis.beck.jetmusicbox.networking.responses.PlaylistResponse
 
-sealed class MainState {
+sealed class MainState : UiState {
     object Loading : MainState()
     data class Idle(
         val albums: List<AlbumResponse>? = null,
